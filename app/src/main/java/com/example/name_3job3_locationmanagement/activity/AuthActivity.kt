52b60d139ad4ea220b9846cc44f0acfd8b1aecaa
MD. ASHIK MAHMUD <ashikmahmud0743@gmail.com>
2026-05-27 19:51:23 +0600

    Update AuthActivity.kt

--- app/src/main/java/com/example/name_3job3_locationmanagement/activity/AuthActivity.kt
+++ app/src/main/java/com/example/name_3job3_locationmanagement/activity/AuthActivity.kt
@@ -1,20 +1,32 @@
 package com.example.name_3job3_locationmanagement.activity
 
+import android.Manifest
 import android.content.Intent
+import android.content.IntentSender
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.name_3job3_locationmanagement.databinding.ActivityAuthBinding
 import com.example.name_3job3_locationmanagement.repo.UserRepository
 import com.example.name_3job3_locationmanagement.viewmodle.AuthViewModel
-
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationSettingsRequest
+import com.google.android.gms.location.Priority
 
 class AuthActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityAuthBinding
+
     private val repo = UserRepository()
+
     private val viewModel: AuthViewModel by viewModels<AuthViewModel> {
         object : ViewModelProvider.Factory {
             override fun <T : ViewModel> create(modelClass: Class<T>): T {
@@ -22,15 +34,30 @@ class AuthActivity : AppCompatActivity() {
             }
         }
     }
+    private val locationPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted ->
 
+            if (isGranted) {
+
+                checkLocationSettingsAndProceed()
+
+            } else {
+               Toast.makeText(
+                    this,
+                    "Location permission is required",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityAuthBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-
-        // app start হওয়ার সাথে সাথে email load হয়
+        // remember email
         val prefs = getSharedPreferences("login_prefs", MODE_PRIVATE)
 
         val isRemembered = prefs.getBoolean("remember", false)
@@ -39,75 +66,168 @@ class AuthActivity : AppCompatActivity() {
             binding.email.setText(prefs.getString("email", ""))
             binding.cbRemember.isChecked = true
         }
-
-        // Initial animations for titles
-//        val fadeInSlide = AnimationUtils.loadAnimation(this, R.anim.fade_in_slide)
-//        binding.appTitle.startAnimation(fadeInSlide)
-//        binding.subTitle.startAnimation(fadeInSlide)
-//        binding.appTitle.setOnClickListener {
-//            it.startAnimation(fadeInSlide)
-//        }
-//
-//        // Bounce animation for "Welcome" text on touch
-//        val bounceAnim = AnimationUtils.loadAnimation(this, R.anim.bounce)
-//        binding.tvWelcome.setOnClickListener {
-//            it.startAnimation(bounceAnim)
-//        }
-
+        // LOGIN
         binding.btnLogin.setOnClickListener {
 
             val email = binding.email.text.toString().trim()
             val password = binding.password.text.toString().trim()
 
             if (email.isEmpty() || password.isEmpty()) {
-                Toast.makeText(this, "please fill the box", Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    this,
+                    "Please fill all fields",
+                    Toast.LENGTH_SHORT
+                ).show()
+
                 return@setOnClickListener
             }
-            // SAVE EMAIL HERE
-            val prefs = getSharedPreferences("login_prefs", MODE_PRIVATE)
-            val editor = prefs.edit()
 
+            val editor = prefs.edit()
             if (binding.cbRemember.isChecked) {
+
                 editor.putString("email", email)
                 editor.putBoolean("remember", true)
+
             } else {
+
                 editor.clear()
             }
             editor.apply()
 
             viewModel.login(email, password)
         }
+
+        // REGISTER
         binding.btnRegister.setOnClickListener {
 
             val email = binding.email.text.toString().trim()
             val password = binding.password.text.toString().trim()
 
             if (email.isEmpty() || password.isEmpty()) {
-                Toast.makeText(this, "please fill the box", Toast.LENGTH_SHORT).show()
+
+                Toast.makeText(
+                    this,
+                    "Please fill all fields",
+                    Toast.LENGTH_SHORT
+                ).show()
+
                 return@setOnClickListener
             }
-
             viewModel.register(email, password)
         }
+
+        // REGISTER RESULT
         viewModel.registerResult.observe(this) { (success, message) ->
+
             if (success) {
-                Toast.makeText(this, "Registration Successful", Toast.LENGTH_SHORT).show()
+
+                Toast.makeText(
+                    this,
+                    "Registration Successful",
+                    Toast.LENGTH_SHORT
+                ).show()
+
             } else {
-                Toast.makeText(this, "Registration Failed", Toast.LENGTH_SHORT).show()
+
+                Toast.makeText(
+                    this,
+                    "Registration Failed",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }
+        // LOGIN RESULT
         viewModel.loginResult.observe(this) { (success, message) ->
+
             if (success) {
-                Toast.makeText(this, "Loggin Successful", Toast.LENGTH_SHORT).show()
-                navigateToFriendList()
+
+                Toast.makeText(
+                    this,
+                    "Login Successful",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+                checkLocationPermissionAndProceed()
+
             } else {
-                Toast.makeText(this, "Loggin Failed", Toast.LENGTH_SHORT).show()
+
+                Toast.makeText(
+                    this,
+                    "Login Failed",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }
     }
+
+    // CHECK LOCATION PERMISSION
+    private fun checkLocationPermissionAndProceed() {
+
+        when {
+
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED -> {
+
+                checkLocationSettingsAndProceed()
+            }
+
+            else -> {
+
+                locationPermissionLauncher.launch(
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            }
+        }
+    }
+
+    // CHECK GPS / LOCATION SETTINGS
+    private fun checkLocationSettingsAndProceed() {
+
+        val locationRequest = LocationRequest.Builder(
+            Priority.PRIORITY_HIGH_ACCURACY,
+            1000
+        ).build()
+
+        val builder = LocationSettingsRequest.Builder()
+            .addLocationRequest(locationRequest)
+
+        val client = LocationServices.getSettingsClient(this)
+
+        val task = client.checkLocationSettings(builder.build())
+
+        task.addOnSuccessListener {
+
+            navigateToFriendList()
+        }
+
+        task.addOnFailureListener { exception ->
+
+            if (exception is ResolvableApiException) {
+
+                try {
+
+                    exception.startResolutionForResult(
+                        this,
+                        1001
+                    )
+
+                } catch (sendEx: IntentSender.SendIntentException) {
+
+                    sendEx.printStackTrace()
+                }
+            }
+        }
+    }
+
+    // NAVIGATE
     private fun navigateToFriendList() {
-        val intent = Intent(this, FriendListActivity::class.java)
-        startActivity(intent)
+
+        val intent = Intent(
+            this,
+            FriendListActivity::class.java)  startActivity(intent)
+
         finish()
     }
 }
